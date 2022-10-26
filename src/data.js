6a7e23dc6eb35c61aeb8e11553fb294d04b62a76
Pintu Kumar <8709793486jio@gmail.com>
2022-10-26 20:30:15 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,271 @@
+export const data = {
+  users: [
+    {
+      id: 1,
+      name: 'Pintu Kumar',
+      img_url: 'https://cdn-icons-png.flaticon.com/512/265/265674.png',
+      chats: [
+        {
+          my_message: [
+            {
+              file: '',
+              content:'Currently Busy,I call you later',
+              name: 'Admin',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/265/265674.png',
+            },
+          ],
+          friend_message: [
+            {
+              file: '',
+              content: 'Hello bro',
+              name: 'Solanki',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/3048/3048122.png',
+            },
+            {
+              file: '',
+              content: 'are you there?',
+              name: 'Amit Raghav',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/3048/3048122.png',
+            },
+            {
+              file: '',
+              content: 'I am waiting for your reply',
+              name: 'Sonu Sharma',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/3048/3048122.png',
+            },
+          ],
+        },
+      ],
+      lastText: 'No time for shit',
+    },
+    {
+      id: 2,
+      name: 'Sachin Vihar',
+      img_url: 'https://cdn-icons-png.flaticon.com/512/921/921110.png',
+      chats: [
+        {
+          my_message: [
+            {
+              file: '',
+              content: 'Hi! how are you?',
+              name: 'Admin',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/921/921110.png',
+            },
+            {
+              file: '',
+              content: 'I am fine.How are you?',
+              name: 'Admin',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/921/921110.png',
+            }
+          ],
+          friend_message: [
+            {
+              file: '',
+              content: 'Hello bro!',
+              name: 'Dev Kumar',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/4825/4825096.png',
+            },
+            {
+              file: '',
+              content: '',
+              name: 'Jaikishun',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/4825/4825096.png',
+            },
+            {
+              file: '',
+              content: '',
+              name: 'Nilkamal Singh',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/4825/4825096.png',
+            },
+          ],
+        },
+      ],
+      lastText: 'I am fine bro how are you? ',
+    },
+    {
+      id: 3,
+      name: 'Mohan Singh',
+      img_url: 'https://cdn-icons-png.flaticon.com/512/4825/4825096.png',
+      chats: [
+        {
+          my_message: [
+            {
+              file: '',
+              content: 'Okay Wait..',
+              name: 'Admin',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/64/64572.png',
+            },
+          ],
+          friend_message: [
+            {
+              file: '',
+              content: 'Hi Bro please call me',
+              name: 'Neha Savanur',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/4825/4825096.png',
+            },
+          ],
+        },
+      ],
+      lastText: 'Okay Wait..',
+    },
+    {
+      id: 4,
+      name: 'Priti Sanu',
+      img_url: 'https://cdn-icons-png.flaticon.com/512/4825/4825123.png',
+      chats: [
+        {
+          my_message: [
+            {
+              file: '',
+              content: 'Hi! Shridhar',
+              name: 'Admin',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/64/64572.png',
+            },
+          ],
+          friend_message: [
+            {
+              file: '',
+              content: 'Hey! hi Riya',
+              name: 'Shridhar Shri',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/4825/4825123.png',
+            },
+          ],
+        },
+      ],
+      lastText: 'Hi! Shridhar',
+    },
+    {
+      id: 5,
+      name: 'Nil Savanur',
+      img_url: 'https://cdn-icons-png.flaticon.com/512/4825/4825112.png',
+      chats: [
+        {
+          my_message: [
+            {
+              file: '',
+              content: 'Busy adari niv',
+              name: 'Admin',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/64/64572.png',
+            },
+          ],
+          friend_message: [
+            {
+              file: '',
+              content: 'Ella ri',
+              name: 'Shridhar Shri',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/4825/4825112.png',
+            },
+            {
+              file: '',
+              content: 'Mate yenu samachara',
+              name: 'Shridhar Shri',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/4825/4825112.png',
+            }
+          ],
+        },
+      ],
+      lastText: 'Busy adari niv',
+    },
+    {
+      id: 6,
+      name: 'Shridhar Shri',
+      img_url: 'https://cdn-icons-png.flaticon.com/512/4825/4825044.png',
+      chats: [
+        {
+          my_message: [
+            {
+              file: '',
+              content: 'gharpe hey re call karta ruk',
+              name: 'Admin',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/64/64572.png',
+            },
+          ],
+          friend_message: [
+            {
+              file: '',
+              content: 'Are bhai kaha hey!',
+              name: 'Sachin Kamble',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/4825/4825044.png',
+            },
+          ],
+        },
+      ],
+      lastText: 'gharpe hey re call karta ruk',
+    },
+    {
+      id: 7,
+      name: 'Raju Sonar',
+      img_url: 'https://cdn-icons-png.flaticon.com/512/4825/4825066.png',
+      chats: [
+        {
+          my_message: [
+            {
+              file: '',
+              content: 'kasa aahe bhava',
+              name: 'Admin',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/64/64572.png',
+            },
+          ],
+          friend_message: [
+            {
+              file: '',
+              content: 'kadak',
+              name: 'Santosh Raje',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/4825/4825066.png',
+            },
+          ],
+        },
+      ],
+      lastText: 'kasa aahe bhava',
+    },
+    {
+      id: 8,
+      name: 'Santosh Raje',
+      img_url: 'https://cdn-icons-png.flaticon.com/512/4825/4825072.png',
+      chats: [
+        {
+          my_message: [
+            {
+              file: '',
+              content: 'Kadak',
+              name: 'Admin',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/64/64572.png',
+            },
+          ],
+          friend_message: [
+            {
+              file: '',
+              content: 'Kasa aahe ',
+              
+              name: 'Raju Sonar',
+              img_url:
+                'https://cdn-icons-png.flaticon.com/512/4825/4825072.png',
+            },
+          ],
+        },
+      ],
+      lastText: 'Kadak',
+    },
+  ],
+}
